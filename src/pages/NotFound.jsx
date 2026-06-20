caa2a275f1d49d289108551aad3633f33fa9d73b
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-06-20 12:33:16 +0530

    refactor: Remove commented code and update link class in NotFound component

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,30 +1,3 @@
-// import { Link } from "react-router-dom";
-
-// function NotFound() {
-//   return (
-//     <div style={{ textAlign: "center", padding: "170px 100px" }}>
-//       <h1>404</h1>
-//       <p style={{ marginBottom: "20px" }}>
-//         Oops! The page you're looking for doesn't exist.
-//       </p>
-//       <Link
-//         to="/"
-//         style={{
-//           color: "#ff3c3c",
-//           fontWeight: "bold",
-//           padding: "10px 20px",
-//           letterSpacing: "1.2px",
-//           border: "2px solid #e2b7a7",
-//           borderRadius: "6px",
-//         }}>
-//         Go Back Home
-//       </Link>
-//     </div>
-//   );
-// }
-
-// export default NotFound;
-
 import { Link } from "react-router-dom";
 import { FaTriangleExclamation } from "react-icons/fa6";
 import { FaHome } from "react-icons/fa";
@@ -40,7 +13,7 @@ const NotFound = () => {
           It looks like you've taken a wrong turn. <br />
           Don't worry, let's get you back to class!
         </p>
-        <Link to="/" className="start-btn">
+        <Link to="/" className="browse-btn">
           <FaHome /> Back to Homepage
         </Link>
       </div>
