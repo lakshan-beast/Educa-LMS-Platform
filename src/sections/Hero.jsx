371bd56a84c32acebbdb4a83b6de24c043a90868
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-07-10 13:38:55 +0530

    style: Remove commented-out code in Hero for cleaner implementation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -24,16 +24,9 @@ const Hero = () => {
             <BsStars className="icon" />
             Trusted by 5000+ Students
           </p>
-          {/* <h1>
-            The Official Smart Student Portal For ,
-            <br />
-            <span data-aos="fade-up">Your O/L Exam Success.</span>
-          </h1> */}
-
           <h1>
             LEARN TODAY <span>LEAD TOMORROW</span>
           </h1>
-
           <p data-aos="fade-up">
             Welcome to our official digital learning center for Mathematics,
             Science, and English. Built strictly for our registered students,
@@ -43,16 +36,6 @@ const Hero = () => {
           </p>
 
           <div className="home-buttons" data-aos="fade-right">
-            {/* <a href="#resources" className="started-btn">
-              Get Started Now
-              <svg fill="currentColor" viewBox="0 0 24 24" class="start-icon">
-                <path
-                  clip-rule="evenodd"
-                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
-                  fill-rule="evenodd"></path>
-              </svg>
-            </a> */}
-
             <a href="#resources" className="started-btn">
               Get started Now
               <div class="icon">
@@ -69,26 +52,12 @@ const Hero = () => {
               </div>
             </a>
 
-            {/* <button class="button" type="button">
-              Apply Now
-              <svg fill="currentColor" viewBox="0 0 24 24" class="icon">
-                <path
-                  clip-rule="evenodd"
-                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm4.28 10.28a.75.75 0 000-1.06l-3-3a.75.75 0 10-1.06 1.06l1.72 1.72H8.25a.75.75 0 000 1.5h5.69l-1.72 1.72a.75.75 0 101.06 1.06l3-3z"
-                  fill-rule="evenodd"></path>
-              </svg>
-            </button> */}
-
             <Link to="/result-hub" className=" result-btn">
               <span class="btn-text-one">Year's Results</span>
               <span class="btn-text-two">(2025)</span>
             </Link>
           </div>
         </div>
-
-        {/* <div className="column hero-img" data-aos="zoom-in">
-          <img src={heroImg} loading="lazy" alt="Master O/L with Educa" />
-        </div> */}
       </div>
     </section>
   );
